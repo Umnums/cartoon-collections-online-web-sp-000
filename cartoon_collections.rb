db8fccc5eb050f343f9b10bd0782ad9869bedb81
Joseph Kao <josephtkao@gmail.com>
2020-04-08 19:26:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(name)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |element|
+    element.capitalize + " !"
 end
 
 def long_planeteer_calls# code an argument here
