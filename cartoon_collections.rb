8eaa5a8dd71600e31018e1e8b903d4e3d0e2d98c
Joseph Kao <josephtkao@gmail.com>
2020-04-08 19:28:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,7 @@ end
 def long_planeteer_calls(array)
   array.all do |word|
     word.length < 4
-  end# code an argument here
-  # Your code here
+  end
 end
 
 def find_the_cheese# code an argument here
